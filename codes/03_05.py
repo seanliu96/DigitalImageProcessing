b1296715af708559dfb88f921d5eaf20f82ad63a
Sean Liu <seanliu96@outlook.com>
2017-03-26 00:32:06 +0800

    chapter3

--- codes/03_05.py
+++ codes/03_05.py
@@ -5,26 +5,30 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-def my_scale(image):
+def my_scale1(image):
+    scaled_image = np.frompyfunc(lambda x: max(0, min(x, 255)), 1, 1)(image).astype(np.uint8)
+    return scaled_image
+
+def my_scale2(image):
     scaled_image = image.copy()
     scaled_image = (scaled_image - scaled_image.min()) * 255 / (scaled_image.max() - scaled_image.min())
     return scaled_image.astype(np.uint8)
 
 def my_spatial_masking(image, mask):
-    filtered_image = np.zeros(image.shape, dtype=np.int32)
-    width, height = image.shape
-    w, h = mask.shape
+    filtered_image = np.zeros(image.shape, dtype=np.float64)
+    height, width = image.shape
+    h, w = mask.shape
     mid_w = w // 2
     mid_h = h // 2
-    for x in range(width):
-        for y in range(height):
-            for i in range(w):
-                for j in range(h):
-                    px = x + i - mid_w
-                    py = y + j - mid_h
-                    if px >= 0 and px < width \
-                    and py >= 0 and py < height:
-                        filtered_image[x,y] += mask[i,j] * image[px, py]
+    print(mid_w, mid_h)
+    for x in range(height):
+        for y in range(width):
+            for i in range(h):
+                for j in range(w):
+                    px = x + i - mid_h
+                    py = y + j - mid_w
+                    if px >= 0 and px < height and py >= 0 and py < width:
+                        filtered_image[x, y] += mask[i,j] * image[px, py]
     return filtered_image
 
 
@@ -33,19 +37,17 @@ laplacian_mask = np.array([ [-1, -1, -1],
                             [-1, 8, -1], 
                             [-1, -1, -1]])
 filtered_img = my_spatial_masking(img, laplacian_mask)
-scaled_filtered_img = my_scale(filtered_img)
 new_img = img + filtered_img
-
 plt.subplot(2,2,1)
 plt.title('original image')
 plt.imshow(img, cmap='gray')
 plt.subplot(2,2,2)
 plt.title('laplacian-filtered image')
-plt.imshow(filtered_img, cmap='gray')
+plt.imshow(my_scale1(filtered_img), cmap='gray')
 plt.subplot(2,2,3)
 plt.title('scaled laplacian-filtered-image')
-plt.imshow(scaled_filtered_img, cmap='gray')
+plt.imshow(my_scale2(filtered_img), cmap='gray')
 plt.subplot(2,2,4)
 plt.title('final image')
-plt.imshow(new_img, cmap='gray')
+plt.imshow(my_scale1(new_img), cmap='gray')
 plt.show()
--- codes/03_06.py
+++ codes/03_06.py
@@ -5,25 +5,26 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
+def my_scale(image):
+    scaled_image = np.frompyfunc(lambda x: max(0, min(x, 255)), 1, 1)(image).astype(np.uint8)
+    return scaled_image
+
 def my_spatial_masking(image, mask):
-    filtered_image = np.zeros(image.shape, dtype=np.int32)
-    width, height = image.shape
-    w, h = mask.shape
+    filtered_image = np.zeros(image.shape, dtype=np.float64)
+    height, width = image.shape
+    h, w = mask.shape
     mid_w = w // 2
     mid_h = h // 2
-    for x in range(width):
-        for y in range(height):
-            for i in range(w):
-                for j in range(h):
-                    px = x + i - mid_w
-                    py = y + j - mid_h
-                    if px >= 0 and px < width \
-                    and py >= 0 and py < height:
-                        filtered_image[x,y] += mask[i,j] * image[px, py]
+    for x in range(height):
+        for y in range(width):
+            for i in range(h):
+                for j in range(w):
+                    px = x + i - mid_h
+                    py = y + j - mid_w
+                    if px >= 0 and px < height and py >= 0 and py < width:
+                        filtered_image[x, y] += mask[i,j] * image[px, py]
     return filtered_image
-
-
-img = np.array(Image.open('../images/images_chapter_03/Fig3.43(a).jpg').convert('L'))
+img = np.array(Image.open('../images/images_chapter_03/Fig3.43(a).jpg').convert('L'), dtype=np.float64)
 laplacian_mask = np.array([ [-1, -1, -1], 
                             [-1, 8, -1], 
                             [-1, -1, -1]])
@@ -31,7 +32,7 @@ filtered_img = my_spatial_masking(img, laplacian_mask)
 new_img_A_0 = filtered_img
 new_img_A_1 = img + filtered_img
 new_img_A_1_7 = 1.7 * img + filtered_img
-
+print(new_img_A_1_7.min(), new_img_A_1_7.max())
 plt.subplot(2,2,1)
 plt.title('original image')
 plt.imshow(img, cmap='gray')
@@ -40,8 +41,8 @@ plt.title('A = 0 image')
 plt.imshow(new_img_A_0, cmap='gray')
 plt.subplot(2,2,3)
 plt.title('A = 1 image')
-plt.imshow(new_img_A_1, cmap='gray')
+plt.imshow(my_scale(new_img_A_1), cmap='gray')
 plt.subplot(2,2,4)
 plt.title('A = 1.7 image')
-plt.imshow(new_img_A_1_7, cmap='gray')
+plt.imshow(my_scale(new_img_A_1_7), cmap='gray')
 plt.show()
